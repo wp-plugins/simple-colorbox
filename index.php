6f9bf2c2036731e502064d3ec13216c40eba8057
ryanhellyer <ryanhellyer@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-04-16 17:39:03 +0000

    initial commit
    git-svn-id: https://plugins.svn.wordpress.org/simple-colorbox/trunk@531917 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ index.php
@@ -0,0 +1,156 @@
+<?php
+/*
+
+Plugin Name: Simple Colorbox
+Plugin URI: http://pixopoint.com/products/simple-colorbox/
+Description: A WordPress plugin which adds a Colorbox to your site with no configuration required.
+Author: Ryan Hellyer
+Version: 1.0
+Author URI: http://pixopoint.com/
+
+Copyright (c) 2008 PixoPoint Web Development
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License version 2 as
+published by the Free Software Foundation.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+license.txt file included with this plugin for more information.
+
+
+So yer readin' the source code eh? Apologies if things are a bit messy in here. This is 
+one of the first plugins I ever released, and it showed in the source code. The plugin was
+somewhat of a disaster code wise and I did a massive upgrade of it on April 4th 2012. In
+the interests of not breaking things, I kept a lot of legacy code around which I knew wasn't 
+likely to case any performance or security issues, but has left a huge chunk of ugly looking 
+code in the legacy.php file. There are also bits of similarly ugly code scattered about the
+plugin code. Sorry about that, but I figured it was best to leave it ugly than risk breaking
+things by messing with it. As the saying goes, "If it aint broke, don't fix it!".
+
+If you find any bug fixes, please let me know via ryan@pixopoint.com, but please be aware that
+I am not intending to ever add any more features to this plugin. I'm purely maintaining to ensure
+that existing users have a good experience and don't lose access to the plugin.
+
+*/
+
+
+/**
+ * Define constants
+ * 
+ * @since 1.0
+ * @author Ryan Hellyer <ryan@pixopoint.com>
+ */
+define( 'SIMPLECOLORBOX_DIR', dirname( __FILE__ ) . '/' ); // Plugin folder DIR
+define( 'SIMPLECOLORBOX_URL', WP_PLUGIN_URL . '/' . basename( SIMPLECOLORBOX_DIR ) ); // Plugin folder URL
+define( 'SIMPLECOLORBOX_VERSION', '1.0' );
+//define( 'SIMPLECOLORBOX_THEME', '5' ); // Can be used to over-ride the default theme
+//define( 'SIMPLECOLORBOX_OPACITY', '0.2' );
+//define( 'SIMPLECOLORBOX_HEIGHT', '50' );
+
+/**
+ * Instantiate the plugin
+ * 
+ * @copyright Copyright (c), Ryan Hellyer
+ * @author Ryan Hellyer <ryan@pixopoint.com>
+ * @since 1.0
+ */
+new Simple_Colorbox();
+
+/**
+ * Simple Colorbox class
+ * Adds the required CSS and JS files to front-end of the site
+ * 
+ * This class may be abstracted from the plugin and used in your own theme if you prefer.
+ * This can allow you to offer easy to use colorbox functionality without the hassle of 
+ * users needing to install a complicated plugin.
+ * 
+ * @copyright Copyright (c), Ryan Hellyer
+ * @author Ryan Hellyer <ryan@pixopoint.com>
+ * @since 1.0
+ */
+class Simple_Colorbox {
+
+	/**
+	 * Class constructor
+	 * Adds all the methods to appropriate hooks or shortcodes
+	 * 
+	 * @since 1.0
+	 * @author Ryan Hellyer <ryan@pixopoint.com>
+	 */
+	public function __construct() {
+
+		// Add action hooks
+		add_action( 'wp_enqueue_scripts', array( $this, 'external_css' ) );
+		add_action( 'wp_enqueue_scripts', array( $this, 'external_scripts' ) );
+		add_action( 'wp_head',            array( $this, 'simplecolorbox_ad' ) );
+		add_action( 'wp_head',            array( $this, 'inline_scripts' ) );
+
+	}
+
+	/**
+     * Print scripts onto pages
+	 * 
+	 * @since 1.0
+	 * @author Ryan Hellyer <ryan@pixopoint.com>
+     */
+	function external_scripts() {
+
+		wp_enqueue_script(
+			'colorbox',
+			SIMPLECOLORBOX_URL . '/scripts/jquery.colorbox-min.js',
+			array( 'jquery' ),
+			1.0,
+			true
+		);		
+	}
+
+	/**
+     * Print scripts onto pages
+	 * 
+	 * @since 1.0
+	 * @author Ryan Hellyer <ryan@pixopoint.com>
+     */
+	function inline_scripts() {
+
+		// Do definition check - used by themes/plugins to over-ride the default settings
+		if ( ! defined( SIMPLECOLORBOX_OPACITY ) )
+			define( 'SIMPLECOLORBOX_OPACITY', '0.6' );
+		if ( ! defined( SIMPLECOLORBOX_HEIGHT ) )
+			define( 'SIMPLECOLORBOX_HEIGHT', '95' );
+
+		// Colorbox settings
+		echo '<script>jQuery(function($){$("a[href$=\'jpg\'],a[href$=\'jpeg\'],a[href$=\'png\'],a[href$=\'gif\']").colorbox({ maxHeight:\'' . SIMPLECOLORBOX_HEIGHT . '%\',opacity:\'' . SIMPLECOLORBOX_OPACITY . '\'});});</script>';
+	}
+
+	/*
+	 * Adds CSS to front end of site
+	 * 
+	 * @since 1.0
+	 * @author Ryan Hellyer <ryan@pixopoint.com>
+	 */
+	public function external_css() {
+
+		// Do definition check - used by themes/plugins to over-ride the default settings
+		if ( ! defined( SIMPLECOLORBOX_THEME ) )
+			define( 'SIMPLECOLORBOX_THEME', '1' );
+
+		// Load the stylesheet
+		wp_enqueue_style( 'colorbox', SIMPLECOLORBOX_URL . '/themes/theme' . SIMPLECOLORBOX_THEME . '/colorbox.css', false, '', 'screen' );
+	}
+
+	/**
+     * Display notice about the plugin in head
+	 * 
+	 * @since 1.0
+	 * @author Ryan Hellyer <ryan@pixopoint.com>
+     */
+	function simplecolorbox_ad() {
+
+		echo "\n<!-- Multi-level Navigation Plugin v" . SIMPLECOLORBOX_VERSION ." by Ryan Hellyer ... http://pixopoint.com/products/simple-colorbox/ -->\n";
+
+	}
+
+}
+
